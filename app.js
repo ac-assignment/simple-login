d8f8f8011db258942e2c15672759f7ca357b47fa
XianEnLin <blueeye851114@gmail.com>
2021-12-05 18:05:27 +0800

    feat: 完成登入完整功能

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import { v1 as uuidV1 } from 'uuid'
 import hbs from './config/handlebars.js'
 import users from './models/user.js'
 
@@ -7,17 +9,36 @@ const app = express()
 const PORT = process.env.PORT || 3000
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
+app.use(cookieParser())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 
+let errorTokens = []
+
 app.get('/', (req, res) => {
-  res.render('index')
+  const { id } = req.cookies
+  console.log(errorTokens)
+  if (errorTokens.includes(id)) {
+    errorTokens = errorTokens.filter(t => t !== id)
+    res.render('index', { hasError: true })
+  } else {
+    res.render('index')
+  }
 })
+
 app.post('/login', (req, res) => {
   const { email, password } = req.body
-  console.log(users)
   
-  res.render('success')
+  const user = users.find((user) => {
+    return user.email === email && user.password === password
+  })
+  if (user) {
+    res.render('success', { user })
+  } else {
+    const token = uuidV1()
+    errorTokens.push(token)
+    res.cookie('id', token).redirect('/')
+  }
 })
 
 app.listen(PORT, () => {
---
+++ public/javascripts/index.js
@@ -0,0 +1,20 @@
+if (hasError) {
+  toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": 200,
+    "hideDuration": 200,
+    "timeOut": 3000,
+    "extendedTimeOut": 500,
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+  toastr.error('信箱或密碼錯誤')
+}
